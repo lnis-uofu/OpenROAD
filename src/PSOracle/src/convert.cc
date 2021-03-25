33e67fbf7d4d23364ced5fe22d9ea817211df72d
Ashton Snelgrove <ashton.snelgrove@utah.edu>
2021-03-24 21:05:08 -0600

    Handle multiple levels of nets inside a hierarchical component.

--- src/PSOracle/src/convert.cc
+++ src/PSOracle/src/convert.cc
@@ -3,12 +3,70 @@
 #include <sta/Liberty.hh>
 #include <sta/PortDirection.hh>
 
-mockturtle::aig_network *convert(sta::Network *adapter)
+mockturtle::aig_network::signal convertFuncHack(mockturtle::aig_network *ntk, sta::FuncExpr *expr)
 {
-    mockturtle::aig_network *network;
-    sta::Instance *top = adapter->topInstance();
-    // create PI from the terminals
-    std::map<sta::Term*, mockturtle::aig_network::signal> input_terminals;
+  switch (expr->op()) {
+  case sta::FuncExpr::Operator::op_port:
+    if (expr->left() != nullptr) { // hack the left pointer to be an indirect buffer
+      return convertFuncHack(ntk, ports, *(reinterpret_cast<sta::FuncExpr **>(expr->left())));
+    } else { // hack the right pointer to be an already generated pi signal
+      return *(reinterpret_cast<signal *>(expr->right()));
+    }
+  case sta::FuncExpr::Operator::op_not:
+    return ntk->create_not(convertFunc(ntk, ports, expr->left()));
+  case sta::FuncExpr::Operator::op_or:
+    return ntk->create_or(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
+  case sta::FuncExpr::Operator::op_and:
+    return ntk->create_and(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
+  case sta::FuncExpr::Operator::op_xor:
+    return ntk->create_xor(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
+  case sta::FuncExpr::Operator::op_one:
+    return ntk->get_constant(true);
+  case sta::FuncExpr::Operator::op_zero:
+    return ntk->get_constant(false);
+  }
+  return ntk->get_constant(false);
+}
+
+FuncExpr *hackFunc(FuncExpr *expr, std::map<sta::LibertyPort*, sta::FuncExpr**> *drivers;
+) {
+  if (expr->op() == sta::FuncExpr::Operator::op_port) {
+    return expr;
+  } else {
+  switch (expr->op()) {
+  case sta::FuncExpr::Operator::op_port:
+    sta::FuncExpr* func(sta::FuncExpr::Operator::op_not:,
+			reinterpret_cast<FuncExpr*>(drivers->find(expr->port())->second);
+			nullptr,
+			nullptr);
+    return func;
+  case sta::FuncExpr::Operator::op_not:
+    return FuncExpr::makeNot(convertFunc(ntk, ports, expr->left()));
+  case sta::FuncExpr::Operator::op_or:
+    return FuncExpr::makeOr(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
+  case sta::FuncExpr::Operator::op_and:
+    return FuncExpr::makeAnd(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
+  case sta::FuncExpr::Operator::op_xor:
+    return FuncExpr::makeXor(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
+  case sta::FuncExpr::Operator::op_one:
+    return FuncExpr::makeOne();
+  case sta::FuncExpr::Operator::op_zero:
+    return FuncExpr::makeZero();
+  default:
+    return expr; // TODO throw
+  }
+
+}
+
+mockturtle::aig_network *dothething(sta::Network *adapter)
+{
+  mockturtle::aig_network *network;
+
+  std::map<sta::Net*, sta::FuncExpr**> *drivers;
+  sta::Instance *top = adapter->topInstance();
+
+  // create PI from the terminals
+
     sta::InstancePinIterator *pins = adapter->pinIterator(top);
     while (pins->hasNext()) {
       sta::Pin *pin = pins->next();
@@ -27,133 +85,96 @@ mockturtle::aig_network *convert(sta::Network *adapter)
     // TODO memory cleanup
     return network;
 }
+}
+  //
 
-std::map<sta::Term*, mockturtle::aig_network::signal>
-recurse(std::map<sta::Term*, mockturtle::aig_network::signal> input_terminals,
-	mockturtle::aig_network *network,
-	sta::Instance *instance,
-	sta::Network *adapter)
-{
-  if (instance->isLeaf) {
-    sta::LibertyCell *cell = adapter->libertyCell(instance);
+
+  /*
+    // create PI from the terminals
+    std::map<sta::Term*, mockturtle::aig_network::signal> input_terminals;
     sta::InstancePinIterator *pins = adapter->pinIterator(top);
-    std::map<sta::LibertyPort*, mockturtle::aig_network::signal> *input_terminals;
-    // TODO this doesn't handle internal registers/ports
-    // for each input terminal, get the port and build the input signal list.
     while (pins->hasNext()) {
       sta::Pin *pin = pins->next();
       sta::Term *term = adapter->term(pin);
       if (term) {
-	sta::LibertyPort *liberty_port = adapter->libertyPort(pin);
-	if (port->isClock() || !port->direction()->isInput()) continue;
-	mockturtle::aig_network::signal signal = input_terminals->find(term);
-	input_terminal.insert({port, signal});
+	input_terminals.insert({term, network->create_pi(adapter->name(term))});
       }
     }
-    // for each output terminal, extract function, add signal to output map.
-    std::map<sta::Term*, mockturtle::aig_network::signal> output_terminals;
-    pins = adapter->pinIterator(top);
-    while (pins->hasNext()) {
-      sta::Pin *pin = pins->next();
-      sta::Term *term = adapter->term(pin);
-      if (term) {
-	sta::LibertyPort *liberty_port = adapter->libertyPort(pin);
-	if (!port->direction()->isOutput()) continue;
-	mockturtle::aig_network::signal output = convertFunc(ntk, &input_terminals, liberty_port->function());
-	output_terminals.push(term, signal);
-      }
-    }
-    return output_terminals;
-  } else {
-    std::map<sta::Net*, mockturtle::aig_network::signal> nets;
-    for(auto it = input_terminals.begin(); it != input_terminals.end(); it++) {
-      sta::Term *term = it->first;
-      mockturtle::aig_network::signal signal = it->second;
-      nets.push({adapter->net(term), signal});
-    }
-
-    sta::InstanceChildIterator *children =  adapter.childIterator(instance);
-    size_t visits_remaining = children;
-    while(visits_remaining
+    std::map<sta::Net*, mockturtle::aig_network::signal> *output_terminals =
+      recurse(inputs, network, top, adapter);
     for(auto it = output_terminals.begin(); it != output_terminals.end(); it++) {
       sta::Term *term = it->first;
       mockturtle::aig_network::signal signal = it->second;
       network->create_po(signal, adapter->name(term));
     }
-
-
-
-    //
-    // while not all instances have been visited,
-    // find each child that has input pins fully connected to available nets.
-    // recurse on that instance.
-    // take the returned terminal signals, add to the net map.
-    // once done, get the net for each terminal, make the output
-
-    /*
-    while (children->hasNext()) {
-      sta::Instance *child = children.next();
-      recurse(
-    }
-    */
-  }
-
+    // TODO memory cleanup
+    return network;
+  */
 }
 
-mockturtle::aig_network::signal convertFunc(mockturtle::aig_network *ntk,
-					    std::map<sta::LibertyPort*, mockturtle::aig_network::signal> *ports,
-					    sta::FuncExpr *expr)
+
+
+
+void extractLeaf(std::map<sta::Net*, sta::FuncExpr**> drivers,
+	sta::Instance *instance,
+	sta::Network *adapter)
 {
-  switch (expr->op()) {
-  case sta::FuncExpr::Operator::op_port:
-    return ports->find(expr->port())->second;
-  case sta::FuncExpr::Operator::op_not:
-    return ntk->create_not(convertFunc(ntk, ports, expr->left()));
-  case sta::FuncExpr::Operator::op_or:
-    return ntk->create_or(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
-  case sta::FuncExpr::Operator::op_and:
-    return ntk->create_and(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
-  case sta::FuncExpr::Operator::op_xor:
-    return ntk->create_xor(convertFunc(ntk, ports, expr->left()), convertFunc(ntk, ports, expr->right()));
-  case sta::FuncExpr::Operator::op_one:
-    return ntk->get_constant(true);
-  case sta::FuncExpr::Operator::op_zero:
-    return ntk->get_constant(false);
+  sta::LibertyCell *cell = adapter->libertyCell(instance);
+  sta::map<sta::LibertyPort*, sta::FuncExpr**> ports;
+  // TODO this doesn't handle internal registers/ports
+
+  sta::InstancePinIterator *pins = adapter->pinIterator(instance);
+  // map ports to nets
+  while (pins->hasNext()) {
+    sta::Pin *pin = pins->next();
+    sta::LibertyPort *liberty_port = adapter->libertyPort(pin);
+    sta::Net *net = adapter->net(pin);
+    sta::FuncExpr **func = drivers
+    ports->insert({liberty_port, func})
   }
-  return ntk->get_constant(false);
+
+  // map port functions to net drivers
+  pins = adapter->pinIterator(instance);
+  while (pins->hasNext()) {
+    sta::Pin *pin = pins->next();
+    sta::LibertyPort *func = adapter->libertyPort(pin)->function();
+    if (func) {
+      FuncExpr *hacked = hackFunc(func, ports);
+      sta::Net *net = adapter->net(pin);
+      FuncExpr** ref = drivers->get(net);
+      *ref = hacked;
+    }
+  }
 }
 
-// functions on libertycell of potential interest.
-  // typedef Map<LibertyPort*, Sequential*> PortToSequentialMap; // LibertyPort to Sequential register class
-  // PortToSequentialMap port_to_seq_map_;
-  // bool hasInternalPorts();
-  // findLibertyPort(name)
-
-  // bool isLatchData(LibertyPort *port);
-  // is{macro, memory, pad, dontuse, buffer, inverter}
-  // bool hasSequentials() const;
-  // void makeSequential(...)
-  // void bufferPorts(// Return values.
-  // 		   LibertyPort *&input,
-  // 		   LibertyPort *&output) const;
-  //// Find the sequential with the output connected to an (internal) port.
-  // Sequential *outputPortSequential(LibertyPort *port);
-
-mockturtle::aig_network *convert(sta::LibertyCell *cell) {
-  mockturtle::aig_network *ntk;
-
-  sta::LibertyCellPortIterator iterator(cell);
-  std::map<sta::LibertyPort*, mockturtle::aig_network::signal> ports;
-  for (sta::LibertyPort *port = iterator.next(); iterator.hasNext(); port = iterator.next()) {
-    if (port->isClock() || !port->direction()->isInput()) continue;
-    // Generate signals for each input port.
-    ports.insert({port, ntk->create_pi()});
+void extractHierarchical(std::map<sta::Net*, sta::FuncExpr**> drivers,
+			 sta::Instance *instance,
+			 sta::Network *adapter)
+{
+  sta::InstanceChildIterator *childs = adapter->childIterator(instance);
+  // extract each child.
+  while (childs->hasNext()) {
+    sta::Instance *child = childs->next();
+    recurse(drivers, child, adapter);
   }
-  for (sta::LibertyPort *port = iterator.next(); iterator.hasNext(); port = iterator.next()) {
-    if (!port->direction()->isOutput()) continue;
-    // for each output port, generate signals for function.
+}
 
-    ntk->create_po(convertFunc(ntk, &ports, port->function()));
+void recurse(std::map<sta::Net*, sta::FuncExpr**> drivers,
+	sta::Instance *instance,
+	sta::Network *adapter)
+{
+  // instance->netIterator - includes nets that are connect to the instance parent through pins.
+  // for each output terminal, extract function, add signal to output map.
+  sta::InstanceNetIterator *nets = adapter->netIterator(instance);
+  while (nets->hasNext()) {
+    sta::Net *net = nets->next();
+    //insert empty pointer into drivers for each network if does not exists
+    //drivers->insert(
   }
-  return ntk;
+
+  if (instance->isLeaf) {
+    return extractLeaf(drivers, instance, adapter);
+  } else {
+    return extractHierarchical(drivers, instance, adapter);
+  }
 }
